fb039ae258c44177722a601035af75bf89aec97a
kbinani <kbinani.bt@gmail.com>
2016-09-27 21:56:26 +0900

    func CaptureRect

--- screenshot.go
+++ screenshot.go
@@ -6,7 +6,13 @@ import (
 	"image"
 )
 
-func CaptureDisplay(display_index int) (*image.RGBA, error) {
-	rect := GetDisplayBounds(display_index)
+// CaptureDisplay captures whole region of displayIndex'th display.
+func CaptureDisplay(displayIndex int) (*image.RGBA, error) {
+	rect := GetDisplayBounds(displayIndex)
+	return CaptureRect(rect)
+}
+
+// CaptureRect captures specified region of desktop.
+func CaptureRect(rect image.Rectangle) (*image.RGBA, error) {
 	return Capture(rect.Min.X, rect.Min.Y, rect.Dx(), rect.Dy())
 }
