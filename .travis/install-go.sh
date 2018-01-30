7542816b2bee98e3f516a0db33c5091d2be3d9d7
kbinani <kbinani.bt@gmail.com>
2018-01-31 00:26:28 +0900

    CI: Use linux environment except osx target (#5)

--- .travis/install-go.sh
+++ .travis/install-go.sh
@@ -5,9 +5,18 @@
 
 (
 	cd "$HOME"
+	OS=$(uname -s | tr A-Z a-z)
+	ARCH=$(uname -m)
+	if [ "$ARCH" = "x86_64" ]; then
+		ARCH=amd64
+	elif [ "$ARCH" = "i386" ]; then
+		ARCH=386
+	else
+		exit 7
+	fi
 	mkdir -p "cache/$TRAVIS_REPO_SLUG"
-	if [ ! -f "cache/${TRAVIS_REPO_SLUG}/go${GO_VERSION}.darwin-amd64.tar.gz" ]; then
-		wget "https://storage.googleapis.com/golang/go${GO_VERSION}.darwin-amd64.tar.gz" -O "cache/${TRAVIS_REPO_SLUG}/go${GO_VERSION}.darwin-amd64.tar.gz" || exit 2
+	if [ ! -f "cache/${TRAVIS_REPO_SLUG}/go${GO_VERSION}.${OS}-${ARCH}.tar.gz" ]; then
+		wget "https://dl.google.com/go/go${GO_VERSION}.${OS}-${ARCH}.tar.gz" -O "cache/${TRAVIS_REPO_SLUG}/go${GO_VERSION}.${OS}-${ARCH}.tar.gz" || exit 2
 	fi
 
 	if [ ! -f "go${GO_VERSION}/bin/go" ]; then
@@ -15,7 +24,7 @@
 		rm -rf "gobootstrap${GO_VERSION}"
 
 		rm -rf go
-		tar zxf "cache/${TRAVIS_REPO_SLUG}/go${GO_VERSION}.darwin-amd64.tar.gz" || exit 3
+		tar zxf "cache/${TRAVIS_REPO_SLUG}/go${GO_VERSION}.${OS}-${ARCH}.tar.gz" || exit 3
 
 		mv go "go${GO_VERSION}" || exit 4
 		cp -R "go${GO_VERSION}" "gobootstrap${GO_VERSION}" || exit 5
