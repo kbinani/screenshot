ad81a3652e5048b5db43e9833be38a92ef062c7f
kbinani <kbinani.bt@gmail.com>
2021-07-20 20:44:09 +0900

    CI: Use GOHOSTOS env

--- .github/install-go.sh
+++ .github/install-go.sh
@@ -13,8 +13,8 @@
 		exit 7
 	fi
 	mkdir -p "cache"
-	if [ ! -f "cache/go${GO_VERSION}.${RUNNER_OS}-${ARCH}.tar.gz" ]; then
-		wget "https://dl.google.com/go/go${GO_VERSION}.${RUNNER_OS}-${ARCH}.tar.gz" -O "cache/go${GO_VERSION}.${RUNNER_OS}-${ARCH}.tar.gz" || exit 2
+	if [ ! -f "cache/go${GO_VERSION}.${GOHOSTOS}-${ARCH}.tar.gz" ]; then
+		wget "https://dl.google.com/go/go${GO_VERSION}.${GOHOSTOS}-${ARCH}.tar.gz" -O "cache/go${GO_VERSION}.${GOHOSTOS}-${ARCH}.tar.gz" || exit 2
 	fi
 
 	if [ ! -f "go${GO_VERSION}/bin/go" ]; then
@@ -22,7 +22,7 @@
 		rm -rf "gobootstrap${GO_VERSION}"
 
 		rm -rf go
-		tar zxf "cache/go${GO_VERSION}.${RUNNER_OS}-${ARCH}.tar.gz" || exit 3
+		tar zxf "cache/go${GO_VERSION}.${GOHOSTOS}-${ARCH}.tar.gz" || exit 3
 
 		mv go "go${GO_VERSION}" || exit 4
 		cp -R "go${GO_VERSION}" "gobootstrap${GO_VERSION}" || exit 5
