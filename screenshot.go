595b702e825db9074a501a066d2185f3e06ada51
kbinani <kbinani.bt@gmail.com>
2023-08-12 11:08:18 +0900

    Tweak document for CaptureDisplay

--- screenshot.go
+++ screenshot.go
@@ -6,7 +6,7 @@ import (
 	"image"
 )
 
-// CaptureDisplay captures whole region of displayIndex'th display, starts at 0 for main display.
+// CaptureDisplay captures whole region of displayIndex'th display, starts at 0 for primary display.
 func CaptureDisplay(displayIndex int) (*image.RGBA, error) {
 	rect := GetDisplayBounds(displayIndex)
 	return CaptureRect(rect)
