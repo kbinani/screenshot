35b29ddf675da6da8f2a556933282d0a90621725
kbinani <kbinani.bt@gmail.com>
2025-01-13 17:14:14 +0900

    Add variations of GetDisplayBounds func

--- windows.go
+++ windows.go
@@ -6,6 +6,7 @@ import (
 	"errors"
 	"github.com/lxn/win"
 	"image"
+	"runtime"
 	"syscall"
 	"unsafe"
 )
@@ -102,6 +103,10 @@ func NumActiveDisplays() int {
 }
 
 func GetDisplayBounds(displayIndex int) image.Rectangle {
+	return GetDisplayBounds4(displayIndex)
+}
+
+func GetDisplayBounds0(displayIndex int) image.Rectangle {
 	var ctx getMonitorBoundsContext
 	ctx.Index = displayIndex
 	ctx.Count = 0
@@ -109,6 +114,56 @@ func GetDisplayBounds(displayIndex int) image.Rectangle {
 	return image.Rect(
 		int(ctx.Rect.Left), int(ctx.Rect.Top),
 		int(ctx.Rect.Right), int(ctx.Rect.Bottom))
+}
+
+func GetDisplayBounds1(displayIndex int) image.Rectangle {
+	ctx := &getMonitorBoundsContext{}
+	ctx.Index = displayIndex
+	ctx.Count = 0
+	enumDisplayMonitors(win.HDC(0), nil, syscall.NewCallback(getMonitorBoundsCallback), uintptr(unsafe.Pointer(ctx)))
+	return image.Rect(
+		int(ctx.Rect.Left), int(ctx.Rect.Top),
+		int(ctx.Rect.Right), int(ctx.Rect.Bottom))
+}
+
+func GetDisplayBounds2(displayIndex int) image.Rectangle {
+	ctx := new(getMonitorBoundsContext)
+	ctx.Index = displayIndex
+	ctx.Count = 0
+	enumDisplayMonitors(win.HDC(0), nil, syscall.NewCallback(getMonitorBoundsCallback), uintptr(unsafe.Pointer(ctx)))
+	runtime.KeepAlive(ctx)
+	return image.Rect(
+		int(ctx.Rect.Left), int(ctx.Rect.Top),
+		int(ctx.Rect.Right), int(ctx.Rect.Bottom))
+}
+
+func GetDisplayBounds3(displayIndex int) image.Rectangle {
+	ctx := new(getMonitorBoundsContext)
+	ctx.Index = displayIndex
+	ctx.Count = 0
+	ptr := unsafe.Pointer(ctx)
+	enumDisplayMonitors(win.HDC(0), nil, syscall.NewCallback(getMonitorBoundsCallback), uintptr(ptr))
+	runtime.KeepAlive(ptr)
+	return image.Rect(
+		int(ctx.Rect.Left), int(ctx.Rect.Top),
+		int(ctx.Rect.Right), int(ctx.Rect.Bottom))
+}
+
+func GetDisplayBounds4(displayIndex int) image.Rectangle {
+	hmem := win.GlobalAlloc(win.GMEM_MOVEABLE, unsafe.Sizeof(getMonitorBoundsContext{}))
+	defer win.GlobalFree(hmem)
+	ptr := win.GlobalLock(hmem)
+	defer win.GlobalUnlock(hmem)
+
+	(*getMonitorBoundsContext)(ptr).Index = displayIndex
+	(*getMonitorBoundsContext)(ptr).Count = 0
+
+	enumDisplayMonitors(win.HDC(0), nil, syscall.NewCallback(getMonitorBoundsCallback), uintptr(ptr))
+	rect := ((*getMonitorBoundsContext)(ptr)).Rect
+
+	return image.Rect(
+		int(rect.Left), int(rect.Top),
+		int(rect.Right), int(rect.Bottom))
 }
 
 func getDesktopWindow() win.HWND {
